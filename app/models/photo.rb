3fcad3858454e2c17470e3f4d7421cb9fb604e2f
Mafooch <joshuamaffuccio@gmail.com>
2016-02-19 11:24:37 -0500

    Add contents and destroy instance method. Fix find class method

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,11 +1,24 @@
 class Photo
-  attr_accessor :id, :location, :contents
-  # attr_writer :contents
+  attr_accessor :id, :location
+  attr_writer :contents
 
   def initialize params = {}
     @id = params[:_id].to_s if params[:_id]
     if params[:metadata] && params[:metadata][:location]
       @location = Point.new(params[:metadata][:location])
+    end
+  end
+
+  def contents
+    grid_file = Photo.fs_bucket.find_one(_id: BSON::ObjectId.from_string(@id))
+    # NOTE find_one returns a GridFS document whereas find with results in a view
+    # and find with .first results in a BSON doc
+    if grid_file
+      buffer = ""
+      grid_file.chunks.reduce([]) do |x,chunk|
+          buffer << chunk.data.data
+      end
+      return buffer
     end
   end
 
@@ -20,6 +33,7 @@ class Photo
       description = {}
       file = @contents
       gps = EXIFR::JPEG.new(file).gps
+      file.rewind
       # grab gps with exfir gem from file stored in @contents
       @location = Point.new lng: gps.longitude, lat: gps.latitude
       content_type = 'image/jpeg'
@@ -29,6 +43,13 @@ class Photo
       grid_doc_id = Photo.fs_bucket.insert_one grid_file
       @id = grid_doc_id.to_s
     end
+  end
+
+  def destroy
+    grid_file = Photo.fs_bucket.find_one(_id: BSON::ObjectId.from_string(@id))
+    Photo.fs_bucket.delete_one grid_file
+    # two = Photo.fs_bucket.find(_id: BSON::ObjectId.from_string(@id))
+    # binding.pry
   end
 
   def self.mongo_client
@@ -46,8 +67,6 @@ class Photo
 
   def self.find id
     doc = fs_bucket.find(_id: BSON::ObjectId.from_string(id)).first
-    # @id = doc[:_id].to_s
-    # binding.pry
-    Photo.new doc
+    doc.nil? ? nil : Photo.new(doc)
   end
 end
